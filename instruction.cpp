3576ce77cce5867e8531b780e8f082c35bdf5cae
taikishank <124407665+taikishank@users.noreply.github.com>
2025-03-25 18:34:30 -0700

    create format_check
    pair programming

--- instruction.cpp
+++ instruction.cpp
@@ -2,8 +2,8 @@
 
 
 // Constructor
-Instruction::Instruction(std::string lbl, std::string opc, std::string opd)
-    : label(lbl), opcode(opc), operand(opd){}
+Instruction::Instruction(std::string lbl, std::string instr, std::string opd)
+    : label(lbl), instruction(instr), operand(opd){}
 
 void Instruction::writeToListing(std::ofstream &listingFile) const {
     if (!listingFile){
@@ -12,10 +12,31 @@ void Instruction::writeToListing(std::ofstream &listingFile) const {
     }
 
     if (!label.empty()){
-        listingFile << label << "\t" << opcode << "\t" << operand << std::endl;
+        listingFile << label << "\t" << instruction << "\t" << operand << std::endl;
     }
     else{
-        listingFile << "\t" << opcode << "\t" << operand << std::endl; // formatting may be off, won't know until testing
+        listingFile << "\t" << instruction << "\t" << operand << std::endl; // formatting may be off, won't know until testing
     }
     return;
+}
+
+int Instruction::format_check(std::string instruction, std::string operand){
+    if (instruction[0] == '+')
+        return 4;
+    else if (format2_instructions.find(instruction) != format2_instructions.end())
+        return 2;
+    else if (instruction == "RESB")
+        return stoi(operand);
+    else if (instruction == "RESW")
+        return stoi(operand) * 3;
+    else if (instruction == "BYTE")
+        if (operand[0] == 'C')
+            return operand.size() - 3;
+        else if (operand[0] == 'X')
+            return (operand.size() - 3) / 2;
+        else{
+            std::cout << "You're fucking stupid";
+            exit(0);
+        }
+    return 3;
 }
--- instruction.h
+++ instruction.h
@@ -5,18 +5,22 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <set>
 
 class Instruction{
 public:
     std::string label;
     std::string operand;
-    std::string opcode;
+    std::string instruction;
+    std::set<std::string> format2_instructions;
 
     // Constructor
     Instruction(std::string lbl = "", std::string opc = "", std::string opr = "");
 
     // Function to write to a listing file
     void writeToListing(std::ofstream &listingFile) const;
+
+    int format_check(std::string instruction, std::string operand);
 };
 
 #endif
