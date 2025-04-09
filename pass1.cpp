17a0e3bc4c25558ef8e230eee296f9585dfaff2d
Alejandro Pacheco <ale_pach03@hotmail.com>
2025-04-09 00:42:21 -0700

    Test update

--- pass1.cpp
+++ pass1.cpp
@@ -3,35 +3,72 @@
 #include <sstream>
 #include <vector>
 #include <string>
-#include "Instruction.h"
+#include "instruction.h"
 
-void pass1(const std::string &filename, std::vector<Instruction*> &instructions){
-void pass1(const string &filename){
+void pass1(const std::string &filename, std::ofstream &listingFile)
+{
     std::ifstream file(filename);
-    if (!file){
+    if (!file)
+    {
         std::cout << "Unable to open file " << filename << ". Shutting down." << std::endl;
         return;
     }
 
+    int address = 0x0000; // we have to update this with the first instruction (START)
+
     std::string line;
-    while (std::getline(file, line)){
+    while (std::getline(file, line))
+    {
         std::istringstream iss(line);
         std::vector<std::string> words;
         std::string word;
 
-        while (iss >> word){
+        while (iss >> word)
+        {
             words.push_back(word);
         }
 
-        if (words.size() == 2){
-            instructions.push_back(new Instruction("", words[0], words[1]));
+        std::vector<Instruction *> instruction_list;
+        Instruction *instr;
+
+        if (words.size() == 2)
+        {
+            instr = new Instruction("", words[0], words[1], address);
+            address = writeToListing(instr, address, listingFile);
+            instruction_list.push_back(instr);
         }
-        else if (words.size() == 3){
-            instructions.push_back(new Instruction(words[0], words[1], words[2]));
+        else if (words.size() == 3)
+        { // Update symbol table here
+            instr = new Instruction(words[0], words[1], words[2], address);
+            address = writeToListing(instr, address, listingFile);
+            instruction_list.push_back(instr);
         }
-        else{
+        else
+        {
             std::cout << "ERROR: Invalid input format. Line must contain either 2 or 3 words" << std::endl;
             return;
         }
     }
+}
+
+int writeToListing(Instruction *instruction, int current_address, std::ofstream &listingFile)
+{
+    if (!listingFile)
+    {
+        std::cout << "Listing file is not open! Open that shit up dumbass!" << std::endl;
+        return;
+    }
+
+    if (!instruction->label.empty())
+    {
+        int increment = instruction->reserve_address_bytes();
+        listingFile << instruction->address + increment << "\t" << instruction->label << "\t" << instruction->instruction << "\t" << instruction->operand << std::endl;
+        current_address += increment;
+        return current_address;
+    }
+    else
+    {
+        listingFile << instruction->address << "\t\t" << instruction->instruction << "\t" << instruction->operand << std::endl; // formatting may be off, won't know until testing
+    }
+    return;
 }
